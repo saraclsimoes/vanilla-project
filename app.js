318d519e20a6af4db816169ea6ad0d831ca4322b
saraclsimoes <60150284+saraclsimoes@users.noreply.github.com>
2020-03-10 17:42:41 +0000

    Add weather icon

--- app.js
+++ app.js
@@ -35,6 +35,12 @@ function displayTemperature(response) {
   windElement.innerHTML = response.data.wind.speed;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "4b14c8772cfbddef29c4fa4b2dfd3ebd";
